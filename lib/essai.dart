0f0095b52cb7db6a72e76d6eb69ec72bd438cab2
Mirabelle2003 <miradahhede@gmail.com>
2023-04-12 16:59:21 +0200

    Added my project2

--- lib/essai.dart
+++ lib/essai.dart
@@ -1,202 +1,200 @@
-import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:tflite/tflite.dart';
+// import 'dart:io';
 
-class HomePageDeux extends StatefulWidget {
-  const HomePageDeux({Key? key}) : super(key: key);
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:tflite_flutter/tflite_flutter.dart';
 
-  @override
-  _HomePageDeuxState createState() => _HomePageDeuxState();
-}
+// class HomePageDeux extends StatefulWidget {
+//   const HomePageDeux({Key? key}) : super(key: key);
 
-class _HomePageDeuxState extends State<HomePageDeux> {
-  bool loading = true;
-  late File _image;
-  late List _result;
-  final imagepicker = ImagePicker();
+//   @override
+//   _HomePageDeuxState createState() => _HomePageDeuxState();
+// }
 
-  @override
-  void initState() {
-    super.initState();
-    loadModel().then((val) {
-      setState(() {});
-    });
-  }
+// class _HomePageDeuxState extends State<HomePageDeux> {
+//   bool loading = true;
+//   late File _image;
+//   late List _result;
+//   final imagepicker = ImagePicker();
+//   late Interpreter _interpreter;
 
-  Future<void> detectImage(File image) async {
-    var res = await Tflite.runModelOnImage(
-        path: image.path,
-        numResults: 2,
-        threshold: 0.6,
-        imageMean: 127.5,
-        imageStd: 255.0,
-        asynch: true);
-    setState(() {
-      _result = res!;
-      loading = false;
-      print("voici : $_result");
-    });
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     loadModel().then((val) {
+//       setState(() {});
+//     });
+//   }
 
-  Future<void> loadModel() async {
-    try {
-      Tflite.close();
-      var resultant = await Tflite.loadModel(
-          model: 'assets/model_unquant.tflite',
-          labels: "assets/labels.txt",
-          numThreads: 1, // defaults to 1
-          isAsset:
-              true, // defaults to true, set to false to load resources outside assets
-          useGpuDelegate:
-              false // defaults to false, set to true to use GPU delegate
-          );
-      print("resultat $resultant");
-      // await Tflite.loadModel(
-      //     model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
-    } on PlatformException {
-      print('Failed to load model.');
-    } catch (e) {
-      print("erreur : $e");
-    } finally {
-      print("Chargement du modèle terminé.");
-    }
-  }
+//   Future<void> detectImage(File image) async {
+//     try {
+//       var bytes = await image.readAsBytes();
+//       var input = TensorImage.fromBytes(bytes);
+//       var output = List.filled(2, 0).reshape([1, 2]);
 
-  @override
-  void dispose() {
-    Tflite.close();
-    super.dispose();
-  }
+//       _interpreter.run(input, output);
+//       setState(() {
+//         _result = output[0];
+//         loading = false;
+//         print("voici : $_result");
+//       });
+//     } catch (e) {
+//       print('Erreur : $e');
+//     }
+//   }
 
-  Future<void> pickImageCamera() async {
-    try {
-      final imagePicker = ImagePicker();
-      final XFile? file =
-          await imagePicker.pickImage(source: ImageSource.camera);
-      if (file == null) {
-        return;
-      } else {
-        _image = File(file.path);
-        print(_image);
-      }
-      detectImage(_image);
-    } catch (e) {
-      print('Erreur : $e');
-    }
-  }
+//   Future<void> loadModel() async {
+//     try {
+//       var interpreterOptions = InterpreterOptions();
+//       var interpreter = await Interpreter.fromAsset(
+//           'assets/model_unquant.tflite',
+//           options: interpreterOptions);
+//       setState(() {
+//         _interpreter = interpreter;
+//       });
+//       print("Chargement du modèle terminé.");
+//     } on PlatformException {
+//       print('Failed to load model.');
+//     } catch (e) {
+//       print("erreur : $e");
+//     }
+//   }
 
-  Future<void> pickImageGallery() async {
-    try {
-      final imagePicker = ImagePicker();
-      final XFile? file =
-          await imagePicker.pickImage(source: ImageSource.gallery);
-      if (file == null) {
-        return;
-      } else {
-        _image = File(file.path);
-        print(_image);
-      }
-      detectImage(_image);
-    } catch (e) {
-      print('Erreur : $e');
-    }
-  }
+//   @override
+//   void dispose() {
+//     _interpreter.close();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    var h = MediaQuery.of(context).size.height;
-    var w = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'ML Classifier',
-          style: GoogleFonts.roboto(),
-        ),
-      ),
-      body: SizedBox(
-        height: h,
-        width: w,
-        child: Column(
-          children: [
-            Container(
-              height: 150,
-              width: 150,
-              padding: const EdgeInsets.all(10),
-              child: Image.asset('assets/mask.png'),
-            ),
-            Container(
-                child: Text('Mask Detector',
-                    style: GoogleFonts.roboto(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ))),
-            const SizedBox(height: 50),
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.teal[800],
-                      shape: BoxShape.circle,
-                    ),
-                    padding: const EdgeInsets.only(left: 10, right: 10),
-                    height: 50,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        child: Text('Capture',
-                            style: GoogleFonts.roboto(fontSize: 18)),
-                        onPressed: () {
-                          pickImageCamera();
-                        }),
-                  ),
-                  const SizedBox(height: 10),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.teal[800],
-                      shape: BoxShape.circle,
-                    ),
-                    padding: const EdgeInsets.only(left: 10, right: 10),
-                    height: 50,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        child: Text('Gallery',
-                            style: GoogleFonts.roboto(fontSize: 18)),
-                        onPressed: () {
-                          pickImageGallery();
-                        }),
-                  ),
-                ],
-              ),
-            ),
-            loading != true
-                ? Container(
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 220,
-                          // width: double.infinity,
-                          padding: const EdgeInsets.all(15),
-                          child: Image.file(_image),
-                        ),
-                        // _result != null
-                        //     ? Text(
-                        //         (_result[1]['label']).toString().substring(2),
-                        //         style: GoogleFonts.roboto(fontSize: 18))
-                        //     : const Text(''),
-                        // _result != null
-                        //     ? Text('Confidence: ${_result[1]['confidence']}',
-                        //         style: GoogleFonts.roboto(fontSize: 18))
-                        //     : const Text('')
-                      ],
-                    ),
-                  )
-                : Container()
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   Future<void> pickImageCamera() async {
+//     try {
+//       final imagePicker = ImagePicker();
+//       final XFile? file =
+//           await imagePicker.pickImage(source: ImageSource.camera);
+//       if (file == null) {
+//         return;
+//       } else {
+//         _image = File(file.path);
+//         print(_image);
+//       }
+//       detectImage(_image);
+//     } catch (e) {
+//       print('Erreur : $e');
+//     }
+//   }
+
+//   Future<void> pickImageGallery() async {
+//     try {
+//       final imagePicker = ImagePicker();
+//       final XFile? file =
+//           await imagePicker.pickImage(source: ImageSource.gallery);
+//       if (file == null) {
+//         return;
+//       } else {
+//         _image = File(file.path);
+//         print(_image);
+//       }
+//       detectImage(_image);
+//     } catch (e) {
+//       print('Erreur : $e');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var h = MediaQuery.of(context).size.height;
+//     var w = MediaQuery.of(context).size.width;
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'ML Classifier',
+//           style: GoogleFonts.roboto(),
+//         ),
+//       ),
+//       body: SizedBox(
+//         height: h,
+//         width: w,
+//         child: Column(
+//           children: [
+//             Container(
+//               height: 150,
+//               width: 150,
+//               padding: const EdgeInsets.all(10),
+//               child: Image.asset('assets/mask.png'),
+//             ),
+//             Container(
+
+//                 child: Text('Mask Detector',
+//                     style: GoogleFonts.roboto(
+//                       fontSize: 20,
+//                       fontWeight: FontWeight.bold,
+//                     ))),
+//             const SizedBox(height: 50),
+//             Container(
+//               child: Column(
+//                 children: [
+//                   Container(
+//                     decoration: BoxDecoration(
+//                       color: Colors.teal[800],
+//                       shape: BoxShape.circle,
+//                     ),
+//                     padding: const EdgeInsets.only(left: 10, right: 10),
+//                     height: 50,
+//                     width: double.infinity,
+//                     child: ElevatedButton(
+//                         child: Text('Capture',
+//                             style: GoogleFonts.roboto(fontSize: 18)),
+//                         onPressed: () {
+//                           pickImageCamera();
+//                         }),
+//                   ),
+//                   const SizedBox(height: 10),
+//                   Container(
+//                     decoration: BoxDecoration(
+//                       color: Colors.teal[800],
+//                       shape: BoxShape.circle,
+//                     ),
+//                     padding: const EdgeInsets.only(left: 10, right: 10),
+//                     height: 50,
+//                     width: double.infinity,
+//                     child: ElevatedButton(
+//                         child: Text('Gallery',
+//                             style: GoogleFonts.roboto(fontSize: 18)),
+//                         onPressed: () {
+//                           pickImageGallery();
+//                         }),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             loading != true
+//                 ? Container(
+//                     child: Column(
+//                       children: [
+//                         Container(
+//                           height: 220,
+//                           // width: double.infinity,
+//                           padding: const EdgeInsets.all(15),
+//                           child: Image.file(_image),
+//                         ),
+//                         // _result != null
+//                         //     ? Text(
+//                         //         (_result[1]['label']).toString().substring(2),
+//                         //         style: GoogleFonts.roboto(fontSize: 18))
+//                         //     : const Text(''),
+//                         // _result != null
+//                         //     ? Text('Confidence: ${_result[1]['confidence']}',
+//                         //         style: GoogleFonts.roboto(fontSize: 18))
+//                         //     : const Text('')
+//                       ],
+//                     ),
+//                   )
+//                 : Container()
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
